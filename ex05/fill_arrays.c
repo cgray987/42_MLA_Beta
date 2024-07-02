d290911b98554e4316138bac488fd3b891392982
cgray987 <144607180+cgray987@users.noreply.github.com>
2024-07-02 23:51:30 +0200

    norm fix

--- ex05/fill_arrays.c
+++ ex05/fill_arrays.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 21:14:16 by marvin            #+#    #+#             */
-/*   Updated: 2024/07/02 21:19:37 by marvin           ###   ########.fr       */
+/*   Updated: 2024/07/02 21:50:17 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,6 @@ void	fill_right(int size, char arr[64][64])
 
 	limit = 1;
 	row = 0;
-	while (row <= (size - 1) / 2)
-	{
-		i = size - 1;
-		while (i >= size - limit)
-		{
-			arr[row][i] = '*';
-			i -= 2;
-		}
-		limit += 2;
-		row += 2;
-	}
-	limit -= 4;
 	while (row < size)
 	{
 		i = size - 1;
@@ -39,8 +27,29 @@ void	fill_right(int size, char arr[64][64])
 			arr[row][i] = '*';
 			i -= 2;
 		}
-		limit -= 2;
+		if (row < (size - 1) / 2)
+			limit += 2;
+		else
+			limit -= 2;
 		row += 2;
+	}
+}
+
+//sometimes norminette is very dumb
+void	fill_left_bottom(int size, char left[64][64], int limit, int row)
+{
+	int	i;
+
+	while (row < size)
+	{
+		i = limit + 1;
+		while (i >= 0)
+		{
+			left[row][i] = '*';
+			i -= 2;
+		}
+		limit -= 2;
+		row++;
 	}
 }
 
@@ -66,20 +75,10 @@ void	fill_left(int size, char left[64][64])
 		row++;
 	}
 	limit -= 6;
-	while (row < size)
-	{
-		i = limit + 1;
-		while (i >= 0)
-		{
-			left[row][i] = '*';
-			i -= 2;
-		}
-		limit -= 2;
-		row++;
-	}
+	fill_left_bottom(size, left, limit, row);
 }
 
-//creates hourglass shape
+//creates hourglass shape every other row
 void	fill_middle(int size, char arr[64][64])
 {
 	int	i;
